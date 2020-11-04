59e828a77dae69cd7fd539cdf58cda8f2eaaf75f
anugrah1922 <anugrah1922@gmail.com>
2020-11-04 08:56:45 +0700

    wp2

---
+++ per1/perkenalan.php
@@ -0,0 +1,12 @@
+<?php
+class perkenalan extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalan </h1>";
+        echo "Nama saya ADITYA YUDHA REMARAIS<br>
+              Saya tinggal di Mejasem, Tegal, Jawa Tengah, Indonesia <br>
+              Saya suka ngegame";
+
+    }
+}
---
+++ per4/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Aditya Yudha Remarais</li>
+        <li>Nama Belakang : Remarais</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Mejasem, Tegal </li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Favorit</h4>
+    <ul type="square">
+        <li>Bersantai-santai</li>
+        
+    </ul>
+</section>
